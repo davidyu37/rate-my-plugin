9b6d10b21aa73502816a884f5cb84072b0a80c19
David Yu <davidyu37@gmail.com>
2023-07-03 15:06:31 +0800

    comment out

--- app/page.js
+++ app/page.js
@@ -45,44 +45,44 @@ const PluginList = ({ plugins }) => (
 );
 
 export default async function Home() {
-  const [plugins, categories] = await Promise.all([
-    getPlugins(),
-    getCategories(),
-  ]);
-  if (plugins && categories) {
-    const colors = generateContrastingColors(categories.length);
-    const categoryColorMap = {};
+  // const [plugins, categories] = await Promise.all([
+  //   getPlugins(),
+  //   getCategories(),
+  // ]);
+  // if (plugins && categories) {
+  //   const colors = generateContrastingColors(categories.length);
+  //   const categoryColorMap = {};
 
-    const newCategories = categories.map((category, index) => {
-      const categoryName = category["category-name"].toUpperCase();
-      categoryColorMap[categoryName] = colors[index];
-      return {
-        ...category,
-        color: colors[index],
-      };
-    });
+  //   const newCategories = categories.map((category, index) => {
+  //     const categoryName = category["category-name"].toUpperCase();
+  //     categoryColorMap[categoryName] = colors[index];
+  //     return {
+  //       ...category,
+  //       color: colors[index],
+  //     };
+  //   });
 
-    const newPlugins = plugins.map((plugin, index) => {
-      const categoryName = plugin.metadata.category.toUpperCase();
-      return {
-        ...plugin,
-        color: categoryColorMap[categoryName],
-      };
-    });
+  //   const newPlugins = plugins.map((plugin, index) => {
+  //     const categoryName = plugin.metadata.category.toUpperCase();
+  //     return {
+  //       ...plugin,
+  //       color: categoryColorMap[categoryName],
+  //     };
+  //   });
 
-    return (
-      <div className="max-w-screen-md md:mx-auto md:pt-10">
-        <div className="navbar bg-white shadow-md px-2 md:mb-3 md:rounded-lg dark:bg-slate-800">
-          <div className="flex flex-col justify-start select-none rounded-lg px-4 py-2">
-            <span className="text-2xl font-bold">Rate my Plugins</span>
-            <span className="text-xs">Until OpenAI Plugin Store is ready</span>
-          </div>
-        </div>
-        <Filters categories={newCategories} />
-        <PluginList plugins={newPlugins} />
-      </div>
-    );
-  }
+  //   return (
+  //     <div className="max-w-screen-md md:mx-auto md:pt-10">
+  //       <div className="navbar bg-white shadow-md px-2 md:mb-3 md:rounded-lg dark:bg-slate-800">
+  //         <div className="flex flex-col justify-start select-none rounded-lg px-4 py-2">
+  //           <span className="text-2xl font-bold">Rate my Plugins</span>
+  //           <span className="text-xs">Until OpenAI Plugin Store is ready</span>
+  //         </div>
+  //       </div>
+  //       <Filters categories={newCategories} />
+  //       <PluginList plugins={newPlugins} />
+  //     </div>
+  //   );
+  // }
 
   return (
     <div className="max-w-screen-md md:mx-auto md:pt-10">
