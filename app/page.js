001d4b36f494c8e74ff3035b952f2b3b2c4df644
David Yu <davidyu37@gmail.com>
2023-07-03 14:12:53 +0800

    error handling

--- app/page.js
+++ app/page.js
@@ -1,4 +1,3 @@
-
 import "./globals.css";
 import Image from "next/image";
 import PluginCard from "./_components/plugin-card";
@@ -31,33 +30,59 @@ async function getPlugins() {
 const PluginList = ({ plugins }) => (
   <div className="flex flex-wrap bg-white dark:bg-slate-800">
     {plugins.map((plugin, index) => (
-      <PluginCard key={index} id={plugin.id} logoUrl={plugin.metadata.logo_url} url={plugin.metadata.domain} name={plugin.metadata.name} description={plugin.metadata.description} category={plugin.metadata.category} color={plugin.color}/>
+      <PluginCard
+        key={index}
+        id={plugin.id}
+        logoUrl={plugin.metadata.logo_url}
+        url={plugin.metadata.domain}
+        name={plugin.metadata.name}
+        description={plugin.metadata.description}
+        category={plugin.metadata.category}
+        color={plugin.color}
+      />
     ))}
   </div>
 );
 
 export default async function Home() {
-  const [plugins, categories] = await Promise.all([getPlugins(), getCategories()])
+  const [plugins, categories] = await Promise.all([
+    getPlugins(),
+    getCategories(),
+  ]);
+  if (plugins && categories) {
+    const colors = generateContrastingColors(categories.length);
+    const categoryColorMap = {};
 
-  const colors = generateContrastingColors(categories.length)
-  const categoryColorMap = {}
+    const newCategories = categories.map((category, index) => {
+      const categoryName = category["category-name"].toUpperCase();
+      categoryColorMap[categoryName] = colors[index];
+      return {
+        ...category,
+        color: colors[index],
+      };
+    });
 
-  const newCategories = categories.map((category, index) => {
-    const categoryName = category['category-name'].toUpperCase()
-    categoryColorMap[categoryName] = colors[index]
-    return {
-      ...category,
-      color: colors[index]
-    }
-  })
+    const newPlugins = plugins.map((plugin, index) => {
+      const categoryName = plugin.metadata.category.toUpperCase();
+      return {
+        ...plugin,
+        color: categoryColorMap[categoryName],
+      };
+    });
 
-  const newPlugins = plugins.map((plugin, index) => {
-    const categoryName = plugin.metadata.category.toUpperCase()
-    return {
-      ...plugin,
-      color: categoryColorMap[categoryName]
-    }
-  })
+    return (
+      <div className="max-w-screen-md md:mx-auto md:pt-10">
+        <div className="navbar bg-white shadow-md px-2 md:mb-3 md:rounded-lg dark:bg-slate-800">
+          <div className="flex flex-col justify-start select-none rounded-lg px-4 py-2">
+            <span className="text-2xl font-bold">Rate my Plugins</span>
+            <span className="text-xs">Until OpenAI Plugin Store is ready</span>
+          </div>
+        </div>
+        <Filters categories={newCategories} />
+        <PluginList plugins={newPlugins} />
+      </div>
+    );
+  }
 
   return (
     <div className="max-w-screen-md md:mx-auto md:pt-10">
@@ -67,8 +92,7 @@ export default async function Home() {
           <span className="text-xs">Until OpenAI Plugin Store is ready</span>
         </div>
       </div>
-      <Filters categories={newCategories} />
-      <PluginList plugins={newPlugins} />
+      Loading...
     </div>
   );
 }
